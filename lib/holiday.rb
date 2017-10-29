361a1b5f68982b5b23b73e3665597cee255072a3
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-29 11:17:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -56,7 +56,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|
+		puts "#{season.capitalize}:"
+		holiday.each do |holiday, supplies| 
+			puts "  #{holiday.capitalize}: " + supplies.join(', ')	
+		end
+	end
 end
 
 def all_holidays_with_bbq(holiday_hash)
